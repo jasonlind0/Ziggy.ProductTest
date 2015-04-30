ed76deb094bf7df456aeb1802753a1a50eb39d92
jasonlind0 <jason@lind-i.com>
2015-04-30 00:36:54 -0500

    Initial checkin

---
+++ TestResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziggy.ProductTests
+{
+    public class TestResult
+    {
+        public bool Passed { get; set; }
+    }
+}
